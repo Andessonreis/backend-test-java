238e79c1eec4420cb7982d9de12c4452fde09527
andesson reis <andessonreys@gmail.com>
2023-12-26 22:35:57 -0300

    Initial commit

---
+++ vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/VehiclecontrolapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.backend.vehiclecontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclecontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclecontrolapiApplication.class, args);
+	}
+
+}
---
+++ vehiclecontrolapi/src/test/java/br/com/desafio/backend/vehiclecontrolapi/VehiclecontrolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.backend.vehiclecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclecontrolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
