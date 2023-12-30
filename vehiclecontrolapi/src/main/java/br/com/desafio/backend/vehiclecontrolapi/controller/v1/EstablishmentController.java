f0d0c8937f27b6ae994fca61061d200abdea92a2
andesson reis <andessonreys@gmail.com>
2023-12-30 13:07:34 -0300

    ✨feat: Add CRUD Endpoints to EstablishmentController
    Extend the EstablishmentController to include CRUD (Create, Read, Update, Delete) operations for the 'Establishment' entity.
    
    - Implement endpoints for updating and deleting establishments.
    - Utilize EstablishmentService, ObjectMapperUtil, and ResultError for error handling.
    - Introduce @Transactional for the deleteEstablishmentId method to handle transactional operations.

--- vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/controller/v1/EstablishmentController.java
+++ vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/controller/v1/EstablishmentController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +18,9 @@ import br.com.desafio.backend.vehiclecontrolapi.domain.establishment.Establishme
 import br.com.desafio.backend.vehiclecontrolapi.dtos.EstablishmentDto;
 import br.com.desafio.backend.vehiclecontrolapi.infrastructure.util.ObjectMapperUtil;
 import br.com.desafio.backend.vehiclecontrolapi.services.EstablishmentService;
-
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.http.HttpStatus;
 
@@ -45,29 +48,65 @@ public class EstablishmentController {
 
     /**
      * Gets the list of all establishments.
-     *
-     * @return A list of establishments or an error response in case of failure.
+     *<p>
+     * @return ResponseEntity with a list of establishments or an error response in case of failure.
      */
     @GetMapping(path = "/establishments", produces = "application/json")
     public ResponseEntity<?> listEstablishments() {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(establishmentService.listEstablishments());
-    }
 
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(establishmentService.listEstablishments());
+    }
+    
     /**
      * Saves an establishment.
-     *
-     * @return A generic response entity.
+     *<p>
+     * @param establishmentDto The DTO representing the establishment to be saved.
+     * @param result BindingResult for validation.
+     * @return ResponseEntity with a generic response entity or an error response in case of validation failure.
      */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping(path = "/establishments/establishment", consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> saveEstablishment(@RequestBody @Valid EstablishmentDto establishmentDto,
-                                              BindingResult result) {
+                                                BindingResult result) {
         return (result.hasErrors())
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.OK)
                 .body(this.establishmentService.saveEstablishment(this.objectMapperUtil
                         .map(establishmentDto, new Establishment())));
+    }
+
+
+    /**
+     * Updates an establishment and returns a generic response entity.
+     *<p>
+     * @param establishmentDto The DTO representing the establishment to be updated.
+     * @param result BindingResult for validation.
+     * @return ResponseEntity with the updated establishment or an error response in case of validation failure.
+     */
+    @PutMapping(path = "/establishments/establishment", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> updateEstablishment(@Valid @RequestBody EstablishmentDto establishmentDto, BindingResult result) {
+
+        return (result.hasErrors())
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK)
+                .body(this.establishmentService.updateEstablishments(this.objectMapperUtil
+                        .map(establishmentDto, new Establishment())));
+    }
+
+    /**
+     * Deletes an establishment by its ID.
+     *<p>
+     * @param id The ID of the establishment to be deleted.
+     * @return ResponseEntity with a response entity or an error response in case of failure.
+     */
+    @Transactional
+    @DeleteMapping(path = "/establishments/establishment/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> deleteEstablishmentId(@PathVariable("id") @NotNull Long id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(establishmentService.deleteEstablishmentId(id));
     }
 }
