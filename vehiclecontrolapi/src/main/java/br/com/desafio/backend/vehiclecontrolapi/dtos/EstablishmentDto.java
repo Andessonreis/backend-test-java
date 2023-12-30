251998c0ba1e4e4204ae65d8eb37e3dcba667a0b
andesson reis <andessonreys@gmail.com>
2023-12-30 15:10:53 -0300

    ♻️ Refactor EstablishmentDto for enhanced validation and clarity
    Refactored the EstablishmentDto class to improve validation and enhance code clarity:
    
    - Replaced the record structure with Lombok annotations for better control over the class.
    - Utilized Lombok's @Data, @Builder, @NoArgsConstructor, and @AllArgsConstructor annotations.
    - Introduced Jakarta Bean Validation annotations for field-level validation, such as @NotBlank, @NotNull, @Range, @Size, and @Pattern.
    - Adjusted field names to align with conventional naming practices for consistency.

--- vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/dtos/EstablishmentDto.java
+++ vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/dtos/EstablishmentDto.java
@@ -1,30 +1,60 @@
 package br.com.desafio.backend.vehiclecontrolapi.dtos;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Range;
+
+import br.com.desafio.backend.vehiclecontrolapi.domain.address.Address;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
- *
  * @author Andesson Reis
- *
- * DTO para resposta às requisições direcionadas à entidade 'Establishment'.
- * @param name
- * @param CNPJ
- * @param phone
- * @param address
- * @param motorcycleParkingSpaces
- * @param carParkingSpaces
+ * <p>
+ * DTO for responding to requests related to the 'Establishment' entity.
+ * 
+ * @param name                    Establishment name
+ * @param cnpj                    CNPJ (Cadastro Nacional da Pessoa Jurídica) number
+ * @param motorcycleParkingSpaces Number of parking spaces for motorcycles
+ * @param carParkingSpaces        Number of parking spaces for cars
+ * @param address                 Establishment address
+ * @param phone                   Contact phone number
  */
-public record EstablishmentDto(
-    @JsonProperty(value = "name")
-    String name,
-    @JsonProperty(value = "CNPJ")
-    String CNPJ,
-    @JsonProperty(value = "phone")
-    String phone,
-    @JsonProperty(value = "address")
-    AddressDto address,
-    @JsonProperty(value = "motorcycleParkingSpaces")
-    Integer motorcycleParkingSpaces,
-    @JsonProperty(value = "carParkingSpaces")
-    Integer carParkingSpaces
-) {}
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstablishmentDto {
+    private Long id;
+
+    @NotBlank(message = "Value cannot be null or empty")
+    private String name;
+
+    @NotBlank(message = "Value cannot be null or empty")
+    private String cnpj;
+
+    @Column(nullable = false)
+    @NotNull(message = "Value cannot be null or empty")
+    @Range(min = 1, message = "Motorcycle spots must be greater than zero")
+    private Integer motorcycleParkingSpaces;
+
+    @Column(nullable = false)
+    @NotNull(message = "Value cannot be null or empty")
+    @Range(min = 1, message = "Car spots must be greater than zero")
+    private Integer carParkingSpaces;
+
+    @NotNull(message = "Value cannot be null")
+    private Address address;
+
+    @Size(min = 11, max = 11, message = "Value must have a length of 11 characters")
+    @Pattern(regexp = "^[0-9]*", message = "Value must contain only numeric characters")
+    @NotBlank(message = "Value cannot be null")
+    private String phone;
+}
