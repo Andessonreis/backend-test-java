253ffd2c1c7ef4d754004c3f560f0801ee40922d
andesson reis <andessonreys@gmail.com>
2024-01-01 09:06:46 -0300

    ✨feat: Add VehicleType enum
    Introduce the VehicleType enum to represent the types of vehicles.
    
    - Include values for MOTORCYCLE and CAR.

---
+++ vehiclecontrolapi/src/main/java/br/com/desafio/backend/vehiclecontrolapi/domain/vehicles/VehicleType.java
@@ -0,0 +1,18 @@
+package br.com.desafio.backend.vehiclecontrolapi.domain.vehicles;
+
+/**
+ * Enumeration representing the types of vehicles.
+ * 
+ * @author Andesson Reis
+ */
+public enum VehicleType {
+    /**
+     * Represents a motorcycle.
+     */
+    MOTORCYCLE,
+
+    /**
+     * Represents a car.
+     */
+    CAR
+}
